a889c1a291de277014acb7dee6a5aaf60193ede1
Phong Nguyen <nhp1807@gmail.com>
2024-08-23 19:56:54 +0700

    Update code: JWT authentication

---
+++ src/main/java/com/dev/identity_service/controller/AuthenticationController.java
@@ -0,0 +1,43 @@
+package com.dev.identity_service.controller;
+
+import com.dev.identity_service.dto.request.ApiResponse;
+import com.dev.identity_service.dto.request.AuthenticationRequest;
+import com.dev.identity_service.dto.request.IntrospectRequest;
+import com.dev.identity_service.dto.response.AuthenticationResponse;
+import com.dev.identity_service.dto.response.IntrospectResponse;
+import com.dev.identity_service.service.AuthenticationService;
+import com.nimbusds.jose.JOSEException;
+import lombok.RequiredArgsConstructor;
+import lombok.experimental.FieldDefaults;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.text.ParseException;
+
+@RestController
+@RequestMapping("/auth")
+@RequiredArgsConstructor
+@FieldDefaults(makeFinal = true, level = lombok.AccessLevel.PRIVATE)
+public class AuthenticationController {
+    AuthenticationService authenticationService;
+
+    @PostMapping("/token")
+    ApiResponse<AuthenticationResponse> authenticate(@RequestBody AuthenticationRequest request){
+        var result = authenticationService.authenticate(request);
+
+        return ApiResponse.<AuthenticationResponse>builder()
+                .result(result)
+                .build();
+    }
+
+    @PostMapping("/introspect")
+    ApiResponse<IntrospectResponse> authenticate(@RequestBody IntrospectRequest request) throws ParseException, JOSEException {
+        var result = authenticationService.introspect(request);
+
+        return ApiResponse.<IntrospectResponse>builder()
+                .result(result)
+                .build();
+    }
+}
--- src/main/java/com/dev/identity_service/exception/ErrorCode.java
+++ src/main/java/com/dev/identity_service/exception/ErrorCode.java
@@ -6,6 +6,8 @@ public enum ErrorCode {
     USER_EXISTED(1002, "User already exists"),
     USERNAME_INVALID(1003, "Username must be at least 3 characters"),
     PASSWORD_INVALID(1004, "Password must be at least 8 characters"),
+    USER_NOT_EXISTED(1005, "User does not exist"),
+    UNAUTHENTICATED(1005, "User is not authenticated"),
     ;
 
     ErrorCode(int code, String message) {
